477efea31d2d8042f22078a87b6cb45f64c3f29d
Kristopher Brown <kbb8134@rit.edu>
2015-10-11 17:10:16 -0400

    First commit of the Holding.java object file

---
+++ FPTS/src/Holding.java
@@ -0,0 +1,19 @@
+/**
+ * Holding.java
+ * 
+ * $Id: $
+ * 
+ * $Log: $
+ * 
+ */
+
+/**
+ * A financial holding which can be represented as a mutual fund,
+ *     cash in money market accounts, or an equity. 
+ * 
+ * @author kbb8134
+ *
+ */
+public abstract class Holding {
+	
+}
