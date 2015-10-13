a80e09b0fe79368b308130dfa403d734d10e173c
Kristopher Brown <kbb8134@rit.edu>
2015-10-13 15:29:50 -0400

    Created a TrackingSystem class to track all important structures

---
+++ FPTS/src/TrackingSystem.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+
+/**
+ * TrackingSystem.java
+ */
+
+/**
+ * TrackingSystem holds a list of all users, portfolios, and equities that the system knows about. 
+ * 
+ * @author This team saved Olympic dreams with their beam routine
+ */
+public class TrackingSystem {
+
+	private ArrayList<User> uList;
+	private ArrayList<Portfolio> pList;
+	private ArrayList<Equity> eList;
+	
+	public TrackingSystem(){
+		
+	}
+	
+	/**
+	 * Simple adding function
+	 * 
+	 * @param uIn - New user
+	 */
+	public void addUser(User uIn){
+		uList.add(uIn);
+	}
+	
+	/**
+	 * Simple adding function
+	 * 
+	 * @param pIn - new portfolio
+	 */
+	public void addPortfolio(Portfolio pIn){
+		pList.add(pIn);
+	}
+	
+	/**
+	 * Simple adding function
+	 * 
+	 * @param eIn - new equity
+	 */
+	public void addEquity(Equity eIn){
+		eList.add(eIn);
+	}
+	
+	/**
+	 * Simple getter
+	 * 
+	 * @return
+	 */
+	public ArrayList<User> getUsers(){
+		return uList;
+	}
+	
+	/**
+	 * Simple getter
+	 * 
+	 * @return
+	 */
+	public ArrayList<Portfolio> getPortfolios(){
+		return pList;
+	}
+	
+	/**
+	 * Simple getter
+	 * 
+	 * @return
+	 */
+	public ArrayList<Equity> getEquities(){
+		return eList;
+	}
+}
